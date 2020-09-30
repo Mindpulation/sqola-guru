073db9ac321a9b2a1887e5793210aee25d123876
Reza Andriansyah <technoboth@gmail.com>
2020-10-01 00:00:09 +0700

    fix response bad request

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const sec = require('./env/index');
 const cors = require('cors');
 const http = require('http');
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.all("*", (req, res)=>{res.status(304).send({res:"Fuck you!!"})});
+app.all( "*", (req, res)=>{res.send({res:"Fuck you!!"});} );
 
 const server = http.createServer(app);
-server.listen(7777);
+server.listen(sec.port);
