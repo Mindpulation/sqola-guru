aba85beb29bb7873bef3e37bb04cfa4adab86f70
dev-zam <zamsyh.work@gmail.com>
2020-10-08 15:16:45 +0700

    update the router auth to add joi

--- view/auth.js
+++ view/auth.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express.Router();
 const { signupHandle, loginHandle } = require('../controller/mongo');
+const { loginList, signupList } = require('../validator/index');
 
-app.post('/login', loginHandle);
-app.post('/signup', signupHandle);
+
+app.post('/login', loginList, loginHandle);
+app.post('/signup', signupList, signupHandle);
 
 module.exports = app;
